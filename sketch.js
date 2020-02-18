7dc07430ea873568b98bcbc23c201917918d450a
JonFromNYC <82.Garcia@gmail.com>
2020-02-17 21:00:58 -0500

    working demo

--- sketch.js
+++ sketch.js
@@ -1,45 +1,76 @@
-// Declare Rows and Columns
-let grid;
-let cols;
-let rows;
-let resolution = 40;
-
-// Declare functions
-
-function make2Darray(cols, rows) {
+function make2DArray(cols, rows) {
     let arr = new Array(cols);
-    for(let i = 0; i < arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
     }
     return arr;
 }
 
+let grid;
+let cols;
+let rows;
+let resolution = 10;
 
-//set up a 2D grid of zero's and 1's
-function setup(){
-    createCanvas(400,400);
-    cols = width/resolution;
+function setup() {
+    createCanvas(600, 400);
+    cols = width / resolution;
     rows = height / resolution;
-    grid = make2Darray(cols,rows);
+
+    grid = make2DArray(cols, rows);
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             grid[i][j] = floor(random(2));
         }
     }
-}// End of Setup
+}
 
-function draw(){
+function draw() {
     background(0);
 
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let x = i * resolution;
             let y = j * resolution;
-
-            if(grid[i][j] == 1){
+            if (grid[i][j] == 1) {
                 fill(255);
-                rect(x,y,resolution,resolution);
+                stroke(0);
+                rect(x, y, resolution - 1, resolution - 1);
             }
         }
     }
-}// End of draw
+
+    let next = make2DArray(cols, rows);
+
+    // Compute next based on grid
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            // Count live neighbors!
+            let sum = 0;
+            let neighbors = countNeighbors(grid, i, j);
+
+            if (state == 0 && neighbors == 3) {
+                next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
+            }
+        }
+    }
+
+    grid = next;
+}
+
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+            sum += grid[col][row];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
+}
